bc2a41c069b758af64ade173369f795fe0d8432e
abo-sultann <abo-sultann@hotmail.com>
2026-08-28 13:09:16 +0300

    Bump launcher to Stable 1.0.2 ANR fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.aistudio.carlauncher.lzrk26"
     minSdk = 25
     targetSdk = 36
-    versionCode = 10001
-    versionName = "1.0.1"
+    versionCode = 10002
+    versionName = "1.0.2"
     buildConfigField("String", "UPDATE_MANIFEST_URL", "\"https://drive.google.com/uc?export=download&id=15Ki1PBf0_erdbITrYh8k80Kt4rWC5yLi\"")
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
