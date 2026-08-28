d737ce38cf1115a7598162f5dd81150adaa7e3cb
abo-sultann <abo-sultann@hotmail.com>
2026-08-29 02:43:50 +0300

    Add Widget V3 foreground color local

---
+++ app/src/main/java/com/example/ui/components/WidgetVisualLocals.kt
@@ -0,0 +1,6 @@
+package com.example.ui.components
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+val LocalWidgetForegroundColor = staticCompositionLocalOf<Color?> { null }
