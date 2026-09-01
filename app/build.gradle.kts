1f8f0a34883099b49e23ab80a01910d624dee1aa
abo-sultann <abo-sultann@hotmail.com>
2026-09-01 20:33:00 +0300

    Bump Launcher 2026 to 2.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.aistudio.carlauncher.lzrk26"
     minSdk = 25
     targetSdk = 36
-    versionCode = 20003
-    versionName = "2.0.3"
+    versionCode = 20004
+    versionName = "2.0.4"
     buildConfigField("String", "UPDATE_MANIFEST_URL", "\"https://drive.google.com/uc?export=download&id=15Ki1PBf0_erdbITrYh8k80Kt4rWC5yLi\"")
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
