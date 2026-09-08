eafa48965c501b0297f33d9a23556ab9a04a41da
abo-sultann <abo-sultann@hotmail.com>
2026-09-08 14:41:42 +0300

    release: Launcher 2026 2.0.9 Darbak Platform

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.aistudio.carlauncher.lzrk26"
     minSdk = 25
     targetSdk = 36
-    versionCode = 20008
-    versionName = "2.0.8"
+    versionCode = 20009
+    versionName = "2.0.9"
     buildConfigField("String", "UPDATE_MANIFEST_URL", "\"https://drive.google.com/uc?export=download&id=15Ki1PBf0_erdbITrYh8k80Kt4rWC5yLi\"")
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
