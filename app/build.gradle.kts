0ec4a449cb57f7a8d0677cb2d43d4301140df0d3
abo-sultann <abo-sultann@hotmail.com>
2026-09-11 17:10:51 +0300

    release(system): bump Darbak Launcher test build to 2.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.aistudio.carlauncher.lzrk26"
     minSdk = 25
     targetSdk = 36
-    versionCode = 20200
-    versionName = "2.2.0"
+    versionCode = 20201
+    versionName = "2.2.1"
     buildConfigField("String", "UPDATE_MANIFEST_URL", "\"https://drive.google.com/uc?export=download&id=15Ki1PBf0_erdbITrYh8k80Kt4rWC5yLi\"")
     testInstrumentationRunner = "com.example.LauncherReviewRunner"
   }
